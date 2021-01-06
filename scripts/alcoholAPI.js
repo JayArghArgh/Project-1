e1ee0e7a3263f5f723f9f83ba9f45e64866bed40
Blake Sticland <blakelists@yahoo.com.au>
2021-01-06 23:47:45 +1100

    can now store user searches to quicklinks list, remove items from quicklinks list, recalls quicklinks list from local storage and if empty, will load our recommended quicklinks which has the same keywords as the random recipes list

---
+++ scripts/alcoholAPI.js
@@ -0,0 +1,58 @@
+"use strict";
+
+const API_APP_KEY2 = "&apiKey=74f87b20cfd04c5aa17c1f09d672b5fa";
+const API_APP_KEY3 = "&apiKey=c147831ac0e740369f586aebbffd9590"; // second API key for times when 402 error is returned
+// check API_APP_KEY in line 14 below
+const API_PATH2= "https://api.spoonacular.com/food/wine/pairing";
+
+
+function getWinePairing(mainIngredient) {
+    // Gets a bunch of recipes matching the keyword searched.
+    let apiUrl2 = API_PATH2;
+    let apiUrlExtension2 = "?food=" + mainIngredient;
+    let apiNumberOfResults = "&number=2";  // not needed in the call?
+    apiUrl2 += apiUrlExtension2 + API_APP_KEY3;
+
+    // add the Spoonacular suggested Paired Wines to Spare div
+    // The Ajax query itself.
+    $.ajax({
+        url: apiUrl2,
+        method: "GET"
+    }).then(function(response) {
+            // create <ul> for list of paired wines and a pairing text explaining the match
+            let spareDiv = $('#spare');
+            spareDiv.empty();
+            let spareDivList = $('<ul>');
+            $(spareDiv).append($(spareDivList));
+            
+            // validating AJAX response (ie. in case there is no matching wine)
+            if (response.status == "failure" || response.pairingText == "") {
+                let winePairFail = "Could not find a wine pairing for " + mainIngredient;
+                let newP = $('<p>');
+                newP.html(winePairFail);
+                spareDivList.append(newP);
+            }
+                        
+            else {
+                for (var ii = 0; ii < response.pairedWines.length; ii++) {
+                    let wineListItem = $('<li>');
+                    let wineListHeading = $('<h6>');
+                    let wineName = response.pairedWines[ii];
+                    // returning all lowercase wine names so will make first letter uppercase
+                    let wineFirstLetter = wineName.charAt(0);
+                    let wineWithCapitalFL = wineFirstLetter.toUpperCase();
+                    let wineNameCapitalFL = wineName.replace(wineFirstLetter, wineWithCapitalFL);
+                    // adding the list of paired wines to the div list
+                    wineListItem.append(wineListHeading);
+                    wineListHeading.html(wineNameCapitalFL);
+                    spareDivList.append(wineListItem);
+                };
+                // appending the wine pairing text after the list of wines 
+                let wineListPara = $('<p>');
+                wineListPara.html(response.pairingText);
+                spareDivList.append(wineListPara);
+            }
+        });
+    };
+    
+
--- scripts/scripts.js
+++ scripts/scripts.js
@@ -1,24 +1,21 @@
 "use strict";
+
 const API_APP_ID = "&app_id=7cfd493f";
 const API_APP_KEY = "&app_key=e16c4ecac435fcb87d81e33ed0937f0f";
 const API_PATH = "https://api.edamam.com/search";
 
-// Place these next two lines in the index file before the </body>
-// <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
-// <script type="text/javascript" src="scripts/scripts.js"></script>
 
 function getRecipe(mainIngredient) {
     // Gets a bunch of recipes matching the keyword searched.
     let apiUrl = API_PATH;
     let apiUrlExtension = "?q=" + mainIngredient;
     apiUrl += apiUrlExtension + API_APP_ID + API_APP_KEY;
-
+    
     // The Ajax query itself.
     $.ajax({
         url: apiUrl,
         method: "GET"
     }).then(function(response) {
-        console.log(response);
         let recipeResult = $('#recipe-result');
         recipeResult.empty();
         // add the recipe name to the results.
@@ -30,48 +27,3 @@ function getRecipe(mainIngredient) {
     });
 }
 
-// Simple listener for our search button.
-$('#searchBtn').click(function (event) {
-    let recipeObject;
-    event.preventDefault();
-    // When clicked, grab the keyword and send it to the API for a search.
-    recipeObject = $('#keyword').val();
-    getRecipe(recipeObject);
-});
-
-// Listeners for quick links -- simplify in the future perhaps via addEventListener
-$("#ql1").click(function () {
-    const ql1Text = $("#ql1").html();
-    getRecipe(ql1Text);
-});
-$("#ql2").click(function () {
-    const ql1Text = $("#ql2").html();
-    getRecipe(ql1Text);
-});
-$("#ql3").click(function () {
-    const ql1Text = $("#ql3").html();
-    getRecipe(ql1Text);
-});
-$("#ql4").click(function () {
-    const ql1Text = $("#ql4").html();
-    getRecipe(ql1Text);
-});
-$("#ql5").click(function () {
-    const ql1Text = $("#ql5").html();
-    getRecipe(ql1Text);
-});
-$("#ql6").click(function () {
-    const ql1Text = $("#ql6").html();
-    console.log(ql1Text);
-    getRecipe(ql1Text);
-});
-$("#ql7").click(function () {
-    const ql1Text = $("#ql7").html();
-    getRecipe(ql1Text);
-});
-$("#ql8").click(function () {
-    const ql1Text = $("#ql8").html();
-    getRecipe(ql1Text);
-});
-
-// just another change
