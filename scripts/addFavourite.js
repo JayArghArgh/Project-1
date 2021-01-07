2f9e635b334b91bb997c4a0ffcb4d157f84860ff
jayarghargh <jayarghargh@gmail.com>
2021-01-07 21:05:16 +1030

    added favourites to local storage

---
+++ scripts/addFavourite.js
@@ -0,0 +1,41 @@
+"use strict";
+console.log("=== Loaded addFavourites.js ===");
+
+// Check for favourites. If favourites load them.
+let storedFavourites = JSON.parse(localStorage.getItem("favourites"));
+let userFavourites = [];
+let clickedItemId;
+
+function updateFavourites(favouriteItem) {
+    // Add a new favourite and update local storage.
+    if (!userFavourites.includes(favouriteItem)) {
+        // only do the updates if the item does not exist in the favourites array already.
+        userFavourites.push(favouriteItem);
+        localStorage.setItem("favourites", JSON.stringify(userFavourites));
+    }
+}
+
+// If favourites, initialise them.
+if (storedFavourites) {
+    userFavourites = storedFavourites;
+}
+
+// Listen for clicks to favourite button.
+$('#random-recipes').click(function (event) {
+    clickedItemId = userFavourites.length;  // Replace this with the favourite ID.
+    event.preventDefault();
+    // let favouriteItem = event.target;
+    updateFavourites(clickedItemId);
+});
+
+
+// Once clicked get the recipe id.
+
+
+// If the recipe id isn't in the favourites already then add it.
+
+
+// Otherwise do nothing.
+
+
+// if user clicks favourites menu update the main div with the favourites. leave the search form present for the user.
