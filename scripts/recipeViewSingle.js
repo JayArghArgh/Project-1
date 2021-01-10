413febb6ac9dae519c93562e4148b89847251abf
jayarghargh <jayarghargh@gmail.com>
2021-01-10 21:38:03 +1030

    favourite cards added

--- scripts/recipeViewSingle.js
+++ scripts/recipeViewSingle.js
@@ -1,10 +1,42 @@
+"use strict";
+console.log("=== Loaded recipeViewSingle.js ===");
+
 function viewSingleRecipe(recipeId) {
     // View a single recipe.
     // Set required variables
-    let divRow = $('<div>').attr('class', 'row');
-    let divCol = $('<div>').attr('class', 'col s12');
+    let divRow = $('<div>').attr('class', 'container center');
+    let divCol = $('<div>').attr('class', 'col s12 m4 l4');
 
-    divCol.html('<p>henlo ' + recipeId +'</p>');
-    divRow.append(divCol);
+    // Set the API search path.
+    let apiUrl = "r=" + API_PATH;
+    let apiUrlExtension = "?" + recipeId.split("_")[1];
+
+    apiUrl += apiUrlExtension + API_APP_ID + API_APP_KEY;
+
+
+    // The Ajax query itself.
+    $.ajax({
+        url: "https://api.edamam.com/search?q=54197d5c51861eb539d4a8c2f59efbc8" + API_APP_ID + API_APP_KEY ,
+        method: "GET"
+    }).then(function(response) {
+        let qResult = response.hits[0];
+
+        let cardLayout = $('<div class="card">');
+        let cardImageDiv = $('<div class="card-image single-recipe">');
+        let cardImage = $('<img>');
+        let cardSpan = $('<span class="card-title">');
+        let cardContent = '<p>card content my homie</p>';
+        let cardAction = '<a href="#">This is a link</a>';
+
+        console.log(qResult);
+        cardSpan.append(qResult.recipe.label);
+        cardImage.attr('src', 'https://www.edamam.com/web-img/bf3/bf3783ccb2c4c7e4a97ef81652f5be6b.jpg');
+        // cardImage.attr('width', "400px");
+        cardImageDiv.append(cardImage, cardSpan);
+        cardLayout.append(cardImageDiv, cardContent, cardAction);
+
+        divCol.append(cardLayout);
+        divRow.append(divCol);
+    });
     return divRow;
 }
