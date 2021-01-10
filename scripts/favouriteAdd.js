c7f82ce0616051a0aabdcaec4f8f7f77948c3586
jayarghargh <jayarghargh@gmail.com>
2021-01-10 13:02:49 +1030

    swapped table for a collection

--- scripts/favouriteAdd.js
+++ scripts/favouriteAdd.js
@@ -19,43 +19,39 @@ function updateFavourites(recipeItem) {
 function viewFavourites() {
     // Present all favourites in a table format.
     // Set required variables.
+    let favUl = $('<ul class="collection">');
+    let favLi;
+    let favSpan;
+    let favImg;
+    let favContent;
     let favReturn = $('<row>');
-    let favReturnTable = $('<table>');
     let favReturnLink;
-    let tempTr;
-    let tempTd;
 
     // Set the attributes.
-    favReturn.attr('class', 's1 m4 l4');
-    favReturnTable.attr('class', 'centered striped');
-    favReturnTable.attr('border', '1px');
-
-    // Set the table header.
-    favReturnTable.append(
-        '<thead><tr><th>Recipe</th><th>Remove</th><th>Share</th></tr></thead>'
-    )
+    favReturn.attr('class', 's12');
 
     // Loop through each favourite and append to table.
     storedFavourites.forEach(function (favItem){
         favReturnLink = $('<a>',{
-            text: favItem,
+            html: '<i className="material-icons">grade</i>',
             title: 'Your fav',
+            class: 'secondary-content',
             href: recipeLink + favItem.split("#")[1]
         });
+        favImg = $('<img src="images/05.jpg" alt="" class="circle">');
+        favLi = $('<li class="collection-item avatar">');
+        favSpan = $('<span class="title">Title</span>');
+        favContent = $('<p>'+ favItem +'</p>');
 
-        tempTr = $('<tr>');
-        tempTd = $('<td>');
-
-        tempTd.append(favReturnLink);
-
-        tempTr.append(tempTd);
-        tempTr.append('<td><td>');
-        favReturnTable.append(tempTr);
-
+        favLi.append(favImg);
+        favLi.append(favSpan);
+        favLi.append(favContent);
+        favLi.append(favReturnLink);
+        favUl.append(favLi);
     });
 
-    // Append the table to the column and return.
-    favReturn.append(favReturnTable);
+    // Append the list to the column and return.
+    favReturn.append(favUl);
     return favReturn;
 }
 
